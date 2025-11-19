a976533dcc31da05afa77e61210ca295291b081d
vek03 <vektromboni@gmail.com>
2025-11-19 18:33:12 -0300

    test: passing tests

--- src/test/java/com/vekrest/vekgateway/VekgatewayApplicationTests.java
+++ src/test/java/com/vekrest/vekgateway/VekgatewayApplicationTests.java
@@ -1,10 +1,25 @@
 package com.vekrest.vekgateway;
 
+import com.vekrest.vekgateway.config.JwtDecoderConfig;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VekgatewayApplicationTests {
+	@MockitoBean
+	private JwtDecoderConfig jwtDecoderConfig;
+
+	@MockitoBean
+	private ReactiveJwtDecoder reactiveJwtDecoder;
+
+	@Mock
+	private SecurityWebFilterChain securityWebFilterChain;
 
 	@Test
 	void contextLoads() {
