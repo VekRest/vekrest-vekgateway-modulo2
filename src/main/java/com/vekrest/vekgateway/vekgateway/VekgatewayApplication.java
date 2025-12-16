e8f91fc88ae11e3c4ac62f3de6179ee8aade1fac
vek03 <vektromboni@gmail.com>
2025-12-16 18:16:25 -0300

    feat: log init

--- src/main/java/com/vekrest/vekgateway/vekgateway/VekgatewayApplication.java
+++ src/main/java/com/vekrest/vekgateway/vekgateway/VekgatewayApplication.java
@@ -1,13 +1,21 @@
 package com.vekrest.vekgateway.vekgateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VekgatewayApplication {
+public class VekgatewayApplication implements CommandLineRunner {
+	private static final Logger LOG = LoggerFactory.getLogger(VekgatewayApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(VekgatewayApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		LOG.info("VEKREST -> VEKGATEWAY - INICIALIZADO COM SUCESSO!");
+	}
 }
